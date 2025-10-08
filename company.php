a63de9e0a375f560751bd6625dfa162e112216d7
Atsusaka Yuudai <yunndk16@gmail.com>
2025-10-08 21:53:23 +0000

    CSSの読み込み問題を解決し、基本デザインを適用

--- company.php
+++ company.php
@@ -1,3 +1,38 @@
 <?php include 'header.php'; ?>
+
+    <main>
         <h1>会社情報</h1>
-<?php include 'footer.php'; ?>
+
+        <section class="page-section">
+            <h2>経営理念</h2>
+            <p>ここに「経営理念」の文章が入ります。</p>
+        </section>
+
+        <section class="page-section">
+            <h2>会社概要・沿革</h2>
+            <table class="profile-table">
+                <tr>
+                    <th>商号</th>
+                    <td>三池物産株式会社</td>
+                </tr>
+                <tr>
+                    <th>所在地</th>
+                    <td>（ここに本社の住所が入ります）</td>
+                </tr>
+                <tr>
+                    <th>設立</th>
+                    <td>（ここに設立年月日が入ります）</td>
+                </tr>
+                <tr>
+                    <th>資本金</th>
+                    <td>（ここに資本金の額が入ります）</td>
+                </tr>
+                <tr>
+                    <th>事業内容</th>
+                    <td>（ここに事業内容の文章が入ります）</td>
+                </tr>
+            </table>
+        </section>
+
+    </main>
+    <?php include 'footer.php'; ?>
